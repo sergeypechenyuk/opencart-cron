4bbe2776bea8aea0ef3f50e33c5a4b31db0730f5
sergeypechenyuk <sergey.pechenyuk@gmail.com>
2016-03-09 12:07:01 +0300

    Added files via upload
    Изменена логика добавления задач, теперь в начале идет проверка задач на возможность запуска, если задач для запуска нет, то все линки php-файлов и создания соединения в БД не создаются.
    Добавлен механизм передачи параметров в контроллер.

---
+++ admin/cron_tasks.php
@@ -0,0 +1,5 @@
+<?php
+// example: 
+//$cron->call("module/yourmodulename/method", array ("minute" => "*", "hour" => "*", "day" => "*", "dayofweek" => "*", "dayofmonth" => "*"), array("param" => true));
+ 
+//end file
